d0eead3323ad4f5f908d4ab9a358ae8d9eb3c78c
rice7th <rice7th@gmail.com>
2024-04-26 02:09:31 +0200

    5x5 ssaa

--- src/frag.glsl
+++ src/frag.glsl
@@ -1,7 +1,7 @@
 #version 330
 #extension GL_ARB_gpu_shader_fp64 : enable
 
-#define SSAMOUNT 3
+#define SSAMOUNT 5
 
 in vec4 gl_FragCoord;
 uniform vec2 res;
@@ -87,35 +87,70 @@ vec2 get_c(vec2 offset) {
 */
 
 void main() {
-    vec2 c = get_c(vec2(0));
+    // kill me
+    vec2 c_2_2 = get_c(vec2(2));
+    vec2 c_2_1 = get_c(vec2(2,1));
+    vec2 c_2_0 = get_c(vec2(2,0));
+    vec2 c_2m1 = get_c(vec2(2,-1));
+    vec2 c_2m2 = get_c(vec2(2,-2));
+    vec2 c_1_2 = get_c(vec2(1));
+    vec2 c_1_1 = get_c(vec2(1,1));
+    vec2 c_1_0 = get_c(vec2(1,0));
+    vec2 c_1m1 = get_c(vec2(1,-1));
+    vec2 c_1m2 = get_c(vec2(1,-2));
+    vec2 c_0_2 = get_c(vec2(0));
+    vec2 c_0_1 = get_c(vec2(0,1));
+    vec2 c_0_0 = get_c(vec2(0,0));
+    vec2 c_0m1 = get_c(vec2(0,-1));
+    vec2 c_0m2 = get_c(vec2(0,-2));
+    vec2 cm1_2 = get_c(vec2(-1));
+    vec2 cm1_1 = get_c(vec2(-1,1));
+    vec2 cm1_0 = get_c(vec2(-1,0));
+    vec2 cm1m1 = get_c(vec2(-1,-1));
+    vec2 cm1m2 = get_c(vec2(-1,-2));
+    vec2 cm2_2 = get_c(vec2(-2));
+    vec2 cm2_1 = get_c(vec2(-2,1));
+    vec2 cm2_0 = get_c(vec2(-2,0));
+    vec2 cm2m1 = get_c(vec2(-2,-1));
+    vec2 cm2m2 = get_c(vec2(-2,-2));
 
-    vec2 ct = get_c(vec2(0,1));
-    vec2 cb = get_c(vec2(0,-1));
-    vec2 cl = get_c(vec2(-1,0));
-    vec2 cr = get_c(vec2(1,0));
-
-    vec2 ctl = get_c(vec2(-1, 1));
-    vec2 ctr = get_c(vec2(1));
-    vec2 cbl = get_c(vec2(-1));
-    vec2 cbr = get_c(vec2(1, -1));
 
     if (ssaa == 1) {
 
-        vec3 fractal = mandel(c);
-
-        vec3 fractalt = mandel(ct);
-        vec3 fractalb = mandel(cb);
-        vec3 fractall = mandel(cl);
-        vec3 fractalr = mandel(cr);
-
-        vec3 fractaltl = mandel(ctl);
-        vec3 fractaltr = mandel(ctr);
-        vec3 fractalbl = mandel(cbl);
-        vec3 fractalbr = mandel(cbr);
+        vec3 frac_2_2 = mandel(c_2_2);
+        vec3 frac_2_1 = mandel(c_2_1);
+        vec3 frac_2_0 = mandel(c_2_0);
+        vec3 frac_2m1 = mandel(c_2m1);
+        vec3 frac_2m2 = mandel(c_2m2);
+        vec3 frac_1_2 = mandel(c_1_2);
+        vec3 frac_1_1 = mandel(c_1_1);
+        vec3 frac_1_0 = mandel(c_1_0);
+        vec3 frac_1m1 = mandel(c_1m1);
+        vec3 frac_1m2 = mandel(c_1m2);
+        vec3 frac_0_2 = mandel(c_0_2);
+        vec3 frac_0_1 = mandel(c_0_1);
+        vec3 frac_0_0 = mandel(c_0_0);
+        vec3 frac_0m1 = mandel(c_0m1);
+        vec3 frac_0m2 = mandel(c_0m2);
+        vec3 fracm1_2 = mandel(cm1_2);
+        vec3 fracm1_1 = mandel(cm1_1);
+        vec3 fracm1_0 = mandel(cm1_0);
+        vec3 fracm1m1 = mandel(cm1m1);
+        vec3 fracm1m2 = mandel(cm1m2);
+        vec3 fracm2_2 = mandel(cm2_2);
+        vec3 fracm2_1 = mandel(cm2_1);
+        vec3 fracm2_0 = mandel(cm2_0);
+        vec3 fracm2m1 = mandel(cm2m1);
+        vec3 fracm2m2 = mandel(cm2m2);
     
-        gl_FragColor = vec4((fractalt + fractalb + fractall + fractalr + fractaltl + fractaltr + fractalbl + fractalbr) / 9, 1.0);
+        gl_FragColor = vec4(
+            (frac_2_2 + frac_2_1 + frac_2_0 + frac_2m1 + frac_2m2
+            + frac_1_2 + frac_1_1 + frac_1_0 + frac_1m1 + frac_1m2
+            + frac_0_2 + frac_0_1 + frac_0_0 + frac_0m1 + frac_0m2
+            + fracm1_2 + fracm1_1 + fracm1_0 + fracm1m1 + fracm1m2
+            + fracm2_2 + fracm2_1 + fracm2_0 + fracm2m1 + fracm2m2) / 25, 1.0);
     } else {
-        vec3 fractal = mandel(c);
+        vec3 fractal = mandel(c_0_0);
         gl_FragColor = vec4(fractal, 1.0);
     }
 
